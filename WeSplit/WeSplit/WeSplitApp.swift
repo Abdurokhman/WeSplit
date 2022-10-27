627b6606b015219d64e90ae33435727ffb89e856
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-27 23:19:48 +0400

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abdurokhman Mukhanbetov on 23.08.22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
